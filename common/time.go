524db73d6e2dfa39c5f1bf3273cdb2576836228d
horockey <rassudov99@yandex.ru>
2023-10-20 11:41:13 +0300

    feat: time round

--- common/time.go
+++ common/time.go
@@ -5,3 +5,9 @@ import "time"
 func ToStartOfInterval(t time.Time, ivl time.Duration) time.Time {
 	return t.Add(-(time.Duration(t.Nanosecond()) % time.Duration(ivl.Nanoseconds())))
 }
+
+func ToEndOfInterval(t time.Time, ivl time.Duration) time.Time {
+	return t.
+		Add(-(time.Duration(t.Nanosecond()) % time.Duration(ivl.Nanoseconds()))).
+		Add(ivl)
+}
