a32d1624fe9e607635cfde4a6bf348639d589c3d
horockey <rassudov99@yandex.ru>
2023-12-01 19:51:05 +0300

    feat; WithServer() opt

--- prometheus_server/opts.go
+++ prometheus_server/opts.go
@@ -1,7 +1,9 @@
 package prometheus_server
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/horockey/go-toolbox/options"
@@ -13,6 +15,16 @@ func WithShutdownTimeout(t time.Duration) options.Option[Server] {
 			return fmt.Errorf("timeout must be positive, got: %d", t)
 		}
 		target.shutdownTimeout = t
+		return nil
+	}
+}
+
+func WithServer(serv *http.Server) options.Option[Server] {
+	return func(target *Server) error {
+		if serv == nil {
+			return errors.New("givern server is nil")
+		}
+		target.server = serv
 		return nil
 	}
 }
