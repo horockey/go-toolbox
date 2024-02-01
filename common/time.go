f1a1e64ddbae37dc0399f8b7f68d15ed9ef6719e
horockey <rassudov99@yandex.ru>
2024-02-01 17:02:27 +0300

    fix: time rounds

--- common/time.go
+++ common/time.go
@@ -3,11 +3,9 @@ package common
 import "time"
 
 func ToStartOfInterval(t time.Time, ivl time.Duration) time.Time {
-	return t.Add(-(time.Duration(t.Nanosecond()) % time.Duration(ivl.Nanoseconds())))
+	return t.Add(time.Duration(-t.UnixNano() % ivl.Nanoseconds()))
 }
 
 func ToEndOfInterval(t time.Time, ivl time.Duration) time.Time {
-	return t.
-		Add(-(time.Duration(t.Nanosecond()) % time.Duration(ivl.Nanoseconds()))).
-		Add(ivl)
+	return ToStartOfInterval(t, ivl).Add(ivl)
 }
